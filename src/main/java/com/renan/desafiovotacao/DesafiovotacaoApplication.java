17cf47db7c3aaae6996cf4607096ae11e114e474
Renan <renansouza2310@gmail.com>
2025-04-01 13:47:08 -0300

    Cria projeto do desafio

---
+++ src/main/java/com/renan/desafiovotacao/DesafiovotacaoApplication.java
@@ -0,0 +1,13 @@
+package com.renan.desafiovotacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiovotacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiovotacaoApplication.class, args);
+	}
+
+}
